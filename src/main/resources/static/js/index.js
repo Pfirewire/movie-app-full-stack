b95532b35af4f7c6cab3d10276e08acf6e135557
Stephen Pflug <stephen@pflug.us>
2022-10-16 09:02:51 -0500

    added functionality to get trending movies list

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,9 @@
 $(function() {
+    console.log("inside index.js");
 
     const MovieApp = {
         GlobalURLs: {
+            tmdbTrendingUrl: "https://api.themoviedb.org/3/trending/movie/week",
             backendURLPath: $("#base-url").text()
         }
     }
@@ -13,6 +15,16 @@ $(function() {
             let data = await response.json();
             return data.tmdbKey;
         },
+        // gets trending movie list
+        async trendingMovies() {
+            let tmdbKey = await Get.tmdbKey();
+            let response = await fetch(`${MovieApp.GlobalURLs.tmdbTrendingUrl}${tmdbKey}`);
+            let data = await response.json();
+            console.log(data);
+            return data;
+        }
     }
+
+    Get.trendingMovies();
 
 });
