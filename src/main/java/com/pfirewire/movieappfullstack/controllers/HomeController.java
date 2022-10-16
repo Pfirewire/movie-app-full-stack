f16980480151946c4479aad8eacdf30eb39ff57f
Stephen Pflug <stephen@pflug.us>
2022-10-16 08:57:37 -0500

    passed url object to index page to get backend url into js file

--- src/main/java/com/pfirewire/movieappfullstack/controllers/HomeController.java
+++ src/main/java/com/pfirewire/movieappfullstack/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package com.pfirewire.movieappfullstack.controllers;
 
+import com.pfirewire.movieappfullstack.services.Url;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private Url url;
+
     @GetMapping("/")
-    public String landingPage() {
+    public String landingPage(Model model) {
+        model.addAttribute("url", url);
         return "movie/index";
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,18 @@
 $(function() {
 
+    const MovieApp = {
+        GlobalURLs: {
+            backendURLPath: $("#base-url").text()
+        }
+    }
+
+    const Get = {
+        // gets api key
+        async tmdbKey() {
+            let response = await fetch(`${MovieApp.GlobalURLs.backendURLPath}keys`);
+            let data = await response.json();
+            return data.tmdbKey;
+        },
+    }
 
 });
