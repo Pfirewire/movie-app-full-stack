d82b70abaf68559fdfda9b013b6be19b5f7ce6b3
Stephen Pflug <stephen@pflug.us>
2022-10-17 15:44:16 -0500

    fixed bug where movie lists were not populating. updated version

--- src/main/java/com/pfirewire/movieappfullstack/controllers/MovieListController.java
+++ src/main/java/com/pfirewire/movieappfullstack/controllers/MovieListController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -35,7 +32,8 @@ public class MovieListController {
 
 
     @GetMapping("/movie/list/all")
-    public Set<MovieList> getAllMovieLists() {
+
+    public @ResponseBody Set<MovieList> getAllMovieLists() {
         User user = Utils.currentUser();
         Set<MovieList> userMovieLists = listDao.findAllByMembers(user);
         return userMovieLists;
