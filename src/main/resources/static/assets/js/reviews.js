787b0c17d0f141e9273a02a8ac3229495995bf6e
Stephen Pflug <stephen@pflug.us>
2022-11-07 15:13:05 -0600

    changed resizing of rating stars to all be same size

--- src/main/resources/static/assets/js/reviews.js
+++ src/main/resources/static/assets/js/reviews.js
@@ -88,18 +88,18 @@ $(function() {
             if(rating < 0) {
                 for(let i = 1; i < 6; i++) {
                     html += (`
-                        <i class="bi bi-star px-1 fs-3 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
+                        <i class="bi bi-star px-1 fs-4 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
                     `);
                 }
             } else {
                 for(let i = 1; i <= rating; i++) {
                     html += (`
-                        <i class="bi bi-star-fill px-1 fs-1 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
+                        <i class="bi bi-star-fill px-1 fs-4 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
                     `);
                 }
                 for(let i = rating + 1; i < 6; i++) {
                     html += (`
-                        <i class="bi bi-star px-1 fs-3 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
+                        <i class="bi bi-star px-1 fs-4 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
                     `);
                 }
             }
