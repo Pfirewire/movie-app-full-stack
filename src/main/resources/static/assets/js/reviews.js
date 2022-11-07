1d730fd119f5c0444a94464a3423d3e399ba3653
Stephen Pflug <stephen@pflug.us>
2022-11-06 18:54:12 -0600

    functionality loosly working for ratings on review page

--- src/main/resources/static/assets/js/reviews.js
+++ src/main/resources/static/assets/js/reviews.js
@@ -47,7 +47,7 @@ $(function() {
             }
         },
         async movieRating(id) {
-            let response = await fetch(`${MovieApp.GlobalURLs.backendURLPath}rating/${id}`);
+            let response = await fetch(`${MovieApp.Urls.backendPath}rating/${id}`);
             let data = await response.json();
             return data;
         }
@@ -57,16 +57,18 @@ $(function() {
         async allReviews(promise) {
             await promise.then(reviewData => {
                 reviewData.forEach(async function(review) {
-                    let ratingData = await Get.movieRating(review.movie.id);
-                    let rating = await ratingData.json();
-                    Print.singleReview(review, rating, MovieApp.Divs.reviewList);
+                    let rating = await Get.movieRating(review.movie.id);
+                    // let rating = await ratingData.then(res => res);
+                    .singleReview(review, rating, MovieApp.Divs.reviewList);
                 });
             });
         },
         async singleReview(review, rating, div) {
+            let html = await Print.modalRating(review.movie);
             div.append(`
                 <div class="review-wrapper container bg-dark text-light rounded-3">
                     <div class="review-img">
+                        <div>${html}</div>
                         <img src="${review.movie.poster}">
                     </div>
                     <div class="review-content">
@@ -76,29 +78,30 @@ $(function() {
                 </div>
             `);
         },
-        async modalRating(div, movie) {
+        async modalRating(movie) {
             // get rating for movie
             let ratingData = await Get.movieRating(movie.id);
             let rating = ratingData.value;
-            div.empty();
+            let html = "";
             if(rating < 0) {
                 for(let i = 1; i < 6; i++) {
-                    div.append(`
+                    html += (`
                         <i class="bi bi-star px-1 fs-3 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
                     `);
                 }
             } else {
                 for(let i = 1; i <= rating; i++) {
-                    div.append(`
+                    html += (`
                         <i class="bi bi-star-fill px-1 fs-1 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
                     `);
                 }
                 for(let i = rating + 1; i < 6; i++) {
-                    div.append(`
+                    html += (`
                         <i class="bi bi-star px-1 fs-3 modal-rating-star modal-rating-${i}" data-rating-star="${i}" data-movie-id="${movie.id}"></i>
                     `);
                 }
             }
+            return html;
         },
 
     }
