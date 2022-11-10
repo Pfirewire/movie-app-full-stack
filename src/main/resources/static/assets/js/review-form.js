27c61670cc77fee33b10f13ff27f5547ac142afc
Stephen Pflug <stephen@pflug.us>
2022-11-09 20:05:48 -0600

    added MovieApp and Get objects to review-form.js

--- src/main/resources/static/assets/js/review-form.js
+++ src/main/resources/static/assets/js/review-form.js
@@ -1,3 +1,19 @@
 $(function() {
+    console.log("Inside review-form.js");
+
+    const MovieApp = {
+        Urls: {
+            backendPath: $("#base-url").text()
+        }
+    };
+
+    const Get = {
+        async movieRating(movieId, userId) {
+            console.log(userId);
+            let response = await fetch(`${MovieApp.Urls.backendPath}rating/${movieId}/${userId}`);
+            let data = await response.json();
+            return data;
+        }
+    };
 
 });
