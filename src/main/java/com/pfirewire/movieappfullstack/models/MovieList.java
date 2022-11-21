01be146c9cbf467e488cd8cb88b48d4187fa2d77
Stephen Pflug <stephen@pflug.us>
2022-11-20 19:19:49 -0600

    troubleshooting relationships

--- src/main/java/com/pfirewire/movieappfullstack/models/MovieList.java
+++ src/main/java/com/pfirewire/movieappfullstack/models/MovieList.java
@@ -31,7 +31,8 @@ public class MovieList {
             joinColumns = {@JoinColumn(name = "list_id")},
             inverseJoinColumns = {@JoinColumn(name = "movie_id")}
     )
-//    @JsonBackReference(value = "listMovies")
+    @JsonBackReference(value = "listMovies")
+    @JsonIgnore
     private Set<Movie> movies;
 
     @ManyToMany
