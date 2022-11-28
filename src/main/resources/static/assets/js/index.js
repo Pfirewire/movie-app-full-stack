35c5479cbeacd25f4d1e524dc4a450c2eb31a45d
Stephen Pflug <stephen@pflug.us>
2022-11-28 10:53:58 -0600

    removed Carousel from index.js and using Carousel imported from utils instead

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -38,60 +38,9 @@ $(function() {
         }
     }
 
-    // const Carousel = {
-    //     images: null,
-    //     n: null,
-    //     gap: 60,
-    //     theta: null,
-    //     currImage: 0,
-    //     initialize(carouselRoot) {
-    //         // Set variables
-    //         this.images = carouselRoot.children();
-    //         this.n = this.images.length;
-    //         this.theta = 2 * Math.PI / this.n;
-    //
-    //         // Set up carousel and navigation
-    //         this.setupCarousel(this.n, parseFloat($(this.images[0]).css("width")));
-    //         this.setupNavigation();
-    //     },
-    //     rotate(imageIndex) {
-    //         // Rotates carousel to the image index
-    //         MovieIndex.carouselRoot.css("transform", `translateZ(-783.731px) rotateY(${imageIndex * -30}deg)`);
-    //     },
-    //     setupCarousel(n, s) {
-    //         // Sets up and updates carousel css styling
-    //         // Math stuff for transforming our carousel images
-    //         let apothem = s / (2 * Math.tan(Math.PI / n));
-    //         for (let i = 0; i < n; i++) {
-    //             $(this.images[i]).css("padding", `0 ${this.gap}px`);
-    //             $(this.images[i]).css("border-radius", "1em");
-    //             $(this.images[i]).css("transform", `rotateY(${i * 30}deg) translateZ(${apothem}px)`);
-    //         }
-    //         this.rotate(this.currImage);
-    //     },
-    //     setupNavigation() {
-    //         // Sets up event listeners for the buttons to rotate the carousel
-    //         $(".carousel-next").on("click", function() {
-    //             Carousel.currImage++;
-    //             Carousel.rotate(Carousel.currImage);
-    //         });
-    //         $(".carousel-prev").on("click", function() {
-    //             Carousel.currImage--;
-    //             Carousel.rotate(Carousel.currImage);
-    //         });
-    //     },
-    //     spinRight() {
-    //         $("button.carousel-next").trigger("click");
-    //     },
-    //     spinLeft() {
-    //         $("button.carousel-prev").trigger("click");
-    //     }
-    // }
-
     const Events = {
         initialize() {
             $(window).ready(async function() {
-                console.log("window loaded");
                 // await Carousel.initialize(MovieIndex.carouselRoot);
                 $("#loading-div").addClass("d-none");
                 // Spins carousel every 2.4 seconds
