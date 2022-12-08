3dc8f26d200b575dee9267b87ae7c924009e09df
Stephen Pflug <stephen@pflug.us>
2022-12-07 19:17:08 -0600

    created post method to send reset password email

--- src/main/java/com/pfirewire/movieappfullstack/controllers/UserController.java
+++ src/main/java/com/pfirewire/movieappfullstack/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -47,6 +48,12 @@ public class UserController {
     @GetMapping("/pwreset")
     public String resetPasswordForm() {
         return "user/pw-reset";
+    }
+
+    @PostMapping("/pwreset")
+    public String sendResetPasswordEmail(@RequestParam(name = "email") String email) {
+        mailService.passwordReset(email);
+        return "user/pwreset-sent";
     }
 
 }
