c124637c29c8e628efc0b678d7611f3d989cf523
Stephen Pflug <stephen@pflug.us>
2022-12-18 15:43:06 -0600

    added cors configuration source to allow correct domains for live page. updated version in pom.xml

--- src/main/java/com/pfirewire/movieappfullstack/config/SecurityConfiguration.java
+++ src/main/java/com/pfirewire/movieappfullstack/config/SecurityConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 // Security configuration for application
 @Configuration
@@ -17,10 +22,20 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://pfirewire.com", "https://www.pfirewire.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-            .and()
+//                .cors().configurationSource(corsConfigurationSource())
+//            .and()
 //            .csrf()
 //                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()
 //            .and()
