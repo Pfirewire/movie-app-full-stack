c5cf71bb7ea5c94e5460d8ac333460648acff738
Stephen Pflug <stephen@pflug.us>
2022-12-26 08:01:15 -0600

    created profile picture controller with get and set methods

---
+++ src/main/java/com/pfirewire/movieappfullstack/controllers/ProfilePictureController.java
@@ -0,0 +1,36 @@
+package com.pfirewire.movieappfullstack.controllers;
+
+import com.pfirewire.movieappfullstack.models.ProfilePicture;
+import com.pfirewire.movieappfullstack.models.User;
+import com.pfirewire.movieappfullstack.repositories.ProfilePictureRepository;
+import com.pfirewire.movieappfullstack.repositories.UserRepository;
+import com.pfirewire.movieappfullstack.utils.Utils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfilePictureController {
+
+    private final UserRepository userDao;
+
+    private final ProfilePictureRepository profilePictureDao;
+
+    public ProfilePictureController(UserRepository userDao, ProfilePictureRepository profilePictureDao) {
+        this.userDao = userDao;
+        this.profilePictureDao = profilePictureDao;
+    }
+
+    @GetMapping("/user/picture")
+    public ProfilePicture getProfilePicture() {
+        return profilePictureDao.findByUser(userDao.findById(Utils.currentUser().getId()).get());
+    }
+
+    @PostMapping("/user/picture")
+    public ProfilePicture setProfilePicture(@RequestBody ProfilePicture picture) {
+        picture.setUser(userDao.findById(Utils.currentUser().getId()).get());
+        profilePictureDao.save(picture);
+        return picture;
+    }
+}
--- src/main/java/com/pfirewire/movieappfullstack/repositories/ProfilePictureRepository.java
+++ src/main/java/com/pfirewire/movieappfullstack/repositories/ProfilePictureRepository.java
@@ -3,7 +3,9 @@ package com.pfirewire.movieappfullstack.repositories;
 import com.pfirewire.movieappfullstack.models.ProfilePicture;
 import com.pfirewire.movieappfullstack.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfilePictureRepository extends JpaRepository<ProfilePicture, Long> {
     ProfilePicture findByUser(User user);
 }
