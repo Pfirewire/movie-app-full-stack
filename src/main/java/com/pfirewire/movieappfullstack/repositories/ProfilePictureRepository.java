35f4bbee14dcdb07d992aa30d1eda1b289235648
Stephen Pflug <stephen@pflug.us>
2022-12-26 07:55:18 -0600

    created profile picture repository

---
+++ src/main/java/com/pfirewire/movieappfullstack/repositories/ProfilePictureRepository.java
@@ -0,0 +1,9 @@
+package com.pfirewire.movieappfullstack.repositories;
+
+import com.pfirewire.movieappfullstack.models.ProfilePicture;
+import com.pfirewire.movieappfullstack.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfilePictureRepository extends JpaRepository<ProfilePicture, Long> {
+    ProfilePicture findByUser(User user);
+}
