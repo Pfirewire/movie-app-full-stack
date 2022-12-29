209b8483a71d0b3f70a14239ae762116141f67a8
Stephen Pflug <stephen@pflug.us>
2022-12-29 11:43:56 -0600

    cleaning up code in sitebar.js

--- src/main/resources/static/js/sitebar.js
+++ src/main/resources/static/js/sitebar.js
@@ -1,6 +1,19 @@
 $(function() {
-    // On logout click link, submits logout form to log user out
-    $("#site-bar-logout-link").on("click", function() {
-        $("#site-bar-logout-form").submit();
-    });
+
+    const SiteBar = {
+        initialize() {
+            Events.initialize();
+        }
+    };
+
+    const Events = {
+        initialize() {
+            // On logout click link, submits logout form to log user out
+            $("#site-bar-logout-link").on("click", function() {
+                $("#site-bar-logout-form").submit();
+            });
+        }
+    };
+
+    SiteBar.initialize();
 });
