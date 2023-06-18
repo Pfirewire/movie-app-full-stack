accbd93d56332fb738622fd91cb66289ebc98748
Steve Pflug <stephen@pflug.us>
2023-06-17 20:09:37 -0500

    updating KeyController to serve up keys.js javascript file instead of key object to front end. added script to call controller method in head partial

--- src/main/java/com/pfirewire/movieappfullstack/controllers/KeyController.java
+++ src/main/java/com/pfirewire/movieappfullstack/controllers/KeyController.java
@@ -13,9 +13,34 @@ public class KeyController {
     @Autowired
     private Keys keys;
 
-    @GetMapping("/keys")
-//    @CrossOrigin(origins = {"https://pfirewire.com", "https://www.pfirewire.com"})
-    public Keys getKeys() {
-        return keys;
+    @GetMapping(value="/keys.js", produces = "application/javascript")
+    public String getKeys() {
+//        return keys;
+        return String.format("""
+                const keys = {
+                    unsplashKey: "%s,
+                    filestackKey: %s,
+                    tmdbKey: %s,
+                }
+        """, keys.getUnsplashKey(), keys.getFilestackKey(), keys.getTmdbKey());
     }
 }
+
+//@RestController
+//public class KeysController {
+//    @Autowired
+//    private KeysConfig keys;
+//
+//    @GetMapping(value="/keys.js", produces = "application/javascript")
+//    public String getKeys(){
+//        String theKeys = String.format("""
+//                keys = {
+//                        filestack: "%s",
+//                        mapbox: "%s",
+//                        talkjs: "%s",
+//                }
+//                """, keys.fileStack, keys.mapbox, keys.talkjs);
+//        return theKeys;
+//
+//    }
+//}
